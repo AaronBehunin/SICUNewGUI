626b46e152103ba6d97fde0aaf1e86ca57228144
AaronBehunin <aaron1616161@gmail.com>
2020-09-24 14:07:05 -0600

    Linked graphis with data

--- sicu/src/main/java/sicu/App.java
+++ sicu/src/main/java/sicu/App.java
@@ -53,6 +53,8 @@ public class App
                     System.out.println(" Parsed Array Token  length : " + parsed.length);
 
 
+
+
                     System.out.println("MandatoryType                                                         Alarm                                                           Apnea");
                     System.out.println(  " 115 :  "+parsed[115]+",Low Minute Ventilation : "+ parsed[116] + " ,Alarm  Priority :" +parsed[120]) ;
                     System.out.println("Mandatory Type : "  +parsed[8]);
@@ -74,6 +76,9 @@ public class App
                     System.out.println("PEEP LOW : "+ parsed[61]);
                     System.out.println("Time HIGH : "+ parsed[62]);
 
+
+                    
+
              
 
                     Thread.sleep(1000);
@@ -84,19 +89,17 @@ public class App
         }
     }
     @SuppressWarnings("serial")
-    static class DrawGUI extends JPanel
+    static class DrawGUI extends JPanel 
     {
         private final static int WIDTH = 3000;
         private final static int HEIGHT = 2000;
         public final static int BORDER = 5;
         public final static int FONT_SIZE = 20;
 
-        DrawGUI(){}
+        private Timer timer;
 
-        @Override 
-        public void paintComponent(Graphics g)
+        DrawGUI(String roomName, String oneOneFive,String vent,String priority, String[] RawStrings )
         {
-            super.paintComponent(g);
             setSize(WIDTH, HEIGHT);
             setBackground(Color.BLACK);
 
@@ -105,7 +108,7 @@ public class App
 
             //Set the background of Layout
         // Setting a title label
-        JLabel title = new JLabel("Jony Dip   Adult   12:00");    
+        JLabel title = new JLabel(roomName.toUpperCase());    
         title.setForeground(Color.white);                     //Title Color
         title.setFont(new Font(Font.SANS_SERIF, Font.LAYOUT_LEFT_TO_RIGHT, 20));   //title font
 
@@ -122,16 +125,58 @@ public class App
         this.add(title, c);
 
         // Sets the name
-        this.setName("Jony Dip   Adult   12:00");
+        this.setName(roomName);
 
+        this.setBorder(border);
 
-        //Initializes the alarm hashmap
-        //alarms = new HashMap<String, Alarm>();
-        // Border around the room for the demo
-        
-        //timer to refresh all of the seconds
+        JLabel info = new JLabel("<html>115 :  "+parsed[115]+"<br>Low Minute Ventilation : "+ parsed[116] + "<br>Alarm  Priority :" +parsed[120]+"</html>");
+        info.setForeground(Color.white);  
+        info.setFont(new Font(Font.SANS_SERIF, Font.LAYOUT_LEFT_TO_RIGHT, 20)); 
+        info.setBorder(border);
 
-        }
+        c.fill = GridBagConstraints.BOTH;
+        c.weighty=.25;
+        c.weightx=.5;
+        c.gridx=1;
+        c.gridy=0;
+
+        this.add(info, c);
+
+        String Raw = "<html>Mandatory Type : "  +parsed[8] + "nMode :  "  +parsed[7]+
+        "<br>Spontaneous Type :  "  +parsed[9]+
+        "<br>Trigger type :  "  +parsed[10]+
+        "<br>Respiratory Rate :  "  +parsed[11]+
+        "<br>Tidal Volume (L):  "  +parsed[12]+
+        "<br>Flow :  "  +parsed[13]+
+        "<br>FI02 :  "  +parsed[14]+
+        "<br>Wave Pattern : "  +parsed[30]+
+        "<br>Apnea (s) : "  +parsed[18]+
+        "<br>Inspitory Presssure : "  +parsed[44]+
+        "<br>Presssure Support : "  +parsed[29]+
+        "<br>PEEP : " +parsed[16]+
+        "<br>Flow Trigger : "  +parsed[43]+
+        "<br>Inspitory Time (s): " +parsed[45]+
+        "<br>PEEP HIGH : "+ parsed[60]+
+        "<br>PEEP LOW : "+ parsed[61]+
+        "<br>Time HIGH : "+ parsed[62]+"</html>";
+
+        JLabel raw = new JLabel(Raw);
+        raw.setForeground(Color.white);  
+        raw.setFont(new Font(Font.SANS_SERIF, Font.LAYOUT_LEFT_TO_RIGHT, 20)); 
+        raw.setBorder(border);
+
+        c.weighty=.75;
+        c.gridwidth =2;
+        c.gridx=0;
+        c.gridy=1;
+
+        this.add(raw,c);
+
+        this.validate();
+        this.repaint();
+        setVisible(true);
+
+        }      
     }
     
     public static void main( String[] args ) throws SerialPortException
@@ -140,14 +185,16 @@ public class App
 
         System.out.println("starting serial port");
 
-        Read840 read = new Read840();
-        new Thread(read).start();     
+  //      Read840 read = new Read840();
+  //      new Thread(read).start();     
+
+        String[] Raw = {"a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a"};
         
-        DrawGUI draw = new DrawGUI();
+        DrawGUI draw = new DrawGUI("Joe Somebody Room 4", "OneOneFive", "Vent", "High", Raw);
         JFrame app = new JFrame("Silent ICU");
 
         app.add(draw, BorderLayout.CENTER);
-        app.setSize(3000,2000);
+        app.setSize(1500,800);
         app.setLocationRelativeTo(null);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
